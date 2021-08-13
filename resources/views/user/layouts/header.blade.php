7d96a92dd501b4f156c3a5523b6dea4e3f705d49
D1074181045 <D1074181045@gm.lhu.edu.tw>
2021-08-13 15:44:17 +0800

    [revise] logo style

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container" style="display: table;width: 100%;">
         <div class="logo" style="text-align: center;">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                <img style="height: 80px" src="https://ik.imagekit.io/7bjbvrubevy/title.png" alt="偶像時刻">
-{{--                <img style="height: 80px" src="https://f000.backblazeb2.com/file/idolmoment/title.png" alt="偶像時刻"> --}}
+            <a href="{{ url('/') }}">
+                <img style="height: 100px" src="https://ik.imagekit.io/7bjbvrubevy/logo.svg" alt="偶像時刻">
+{{--                <img style="height: 80px" src="https://f000.backblazeb2.com/file/idolmoment/logo.svg" alt="偶像時刻"> --}}
             </a>
         </div>
     </div>
